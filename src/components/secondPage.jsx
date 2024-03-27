319be0aff0f7272ecdd03852ba334837729f8b96
Tsinjo <tn@alternateeve.com>
2024-03-27 15:28:39 +0300

    second page

---
+++ src/components/secondPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function secondPage() {
+  return (
+    <>
+      <div className="w-full flex flex-col justify-center items-center bg-[#f7e9c7] text-center">
+        <div>
+          <h1 className="font-kinetika_bold text-6xl">
+            Des offres 100% personnalisables
+          </h1>
+        </div>
+        <div>
+          <span className="font-Kinetika_regular text-4xl">
+            Faitre découvrir à votre clientèle des nouvelles saveurs tout en
+            s'adoptant à leur besoin
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
