9de4fec009c85d2d6f37e10bc3a041fcf9aa7c79
Tsinjo <tn@alternateeve.com>
2024-03-27 10:02:09 +0300

    bubble time

--- src/Home.jsx
+++ src/Home.jsx
@@ -23,6 +23,48 @@ export default function Home() {
         <div className="font-kinetika_bold text-6xl">
           Des corners à votre image
         </div>
+        <div className="font-Kinetika_regular text-4xl">
+          Un corner au sein de votre enseigne pour accroître votre clientèle
+          sans augmenter vos frais
+        </div>
+        <div className="flex justify-center">
+          <div className="flex flex-col items-center">
+            <div className="flex justify-center">
+              <img src="" alt="Les Corners" />
+            </div>
+            <h1 className="font-kinetika_bold text-3xl">Les Corners</h1>
+            <div className="w-1/2">
+              <span className="font-Kinetika_regular text-2xl">
+                100% personnalisable, il vous suffit de disposer de au minimum
+                20m2 au sein de votre établissement pour l'accueillir
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="flex justify-center">
+              <img src="" alt="L'équipe " />
+            </div>
+            <h1 className="font-kinetika_bold text-3xl">L'équipe</h1>
+            <div className="w-1/2">
+              <span className="font-Kinetika_regular text-2xl">
+                Nullement besoin de vous lancer dans une campage de recrutement
+                on s'occupe de tout et restons disponible
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="flex justify-center">
+              <img src="" alt="Ecoresponsable" />
+            </div>
+            <h1 className="font-kinetika_bold text-3xl">Ecoresponsable</h1>
+            <div className="w-1/2">
+              <span className="font-Kinetika_regular text-2xl">
+                Dans une démarche RSE, nos fournisseurs, produits et
+                consommables sont écoresponsables et circuits courts
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
